5764fcfee06d4e847ccd4afb51da4ca73eb24a7e
Darko Jovevski <platzmanfan9@gmail.com>
2020-04-14 18:58:32 -0700

    got working the prompt to ask you for id of the product and quantity and it calculated if has enough it prints out the number if it does not have then it print 'Insufficient quantity'

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@
 
 
 var mysql = require("mysql");
-// var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 
 // creating connection object with the localhost and connecting with our database
 
@@ -20,21 +20,69 @@ connection.connect(function(err){
 
     // start();
     console.log("connected as id " + connection.threadId);
+   
     showAllProducts();
-    connection.end();
+    searchProductsId();
+    
+    // connection.end();
     
 });
+
+// listing all the products function that dipslay upon running the app
+
+
 function showAllProducts(){
     connection.query("SELECT * from products", function(err,res){
-        console.log("----------------------------");
+        console.log("\n----------------------------");
         if(err) throw err;
+        console.log("LISTING ALL PRODUCTS FOR SALE\n");
         for (var i =0; i< res.length; i++){
-            console.log("LISTING ALL PRODUCTS FOR SALE");
-            console.log("ID |" +res[i].id + " |  NAME:   | "  + res[i].product_name + " |  PRICE:   | "+ res[i].price);
+            
+            console.log("ID |" +res[i].id + " |  NAME: | "  + res[i].product_name + " |  PRICE: | "+ res[i].price+"$");
         }
     });
 }
 
+// creating the prompts to ask for id and stock how many unit they want
+
+function searchProductsId(){
+    inquirer.
+    prompt([
+        {
+        name: "id",
+        type: "input",
+        message: "Type the ID of the product you would like to buy?"
+    },
+    {
+        name: "stock_quantity",
+        type: "input",
+        message: "How many units you want to buy from this product?"
+    }
+            ])
+    .then(function(answer){
+        console.log(answer.id);  // should be commented 
+        console.log(answer.stock_quantity); // should be commented
+        
+        connection.query("SELECT * FROM products WHERE ?" , {id:answer.id} , function(err,res){
+            if (err) throw err;
+            
+            for (var i = 0;i<res.length; i++){
+           
+                
+            if (res[i].stock_quantity >= answer.stock_quantity){
+                console.log(res[i].product_name);
+                console.log(res[i].stock_quantity - answer.stock_quantity);
+            }
+           else{
+               console.log("Insufficient quantity!");
+           } 
+            
+            }
+            
+        });
+    });
+    
+}
 
 
 
